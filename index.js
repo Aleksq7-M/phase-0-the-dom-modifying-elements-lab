deff7c9f9b3200503a259eb5129af0f1f537ede3
aleksq7 <morrisalex2001@gmail.com>
2021-08-16 14:06:54 -0600

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector('main');
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = '"The Ruler of Everything" is the champion'
+main.remove()
